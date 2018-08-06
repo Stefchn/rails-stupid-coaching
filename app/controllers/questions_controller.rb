b48f8048d7484ddaad70a27cf5e76f50057ea474
Stephane Chen <chn.stephane@gmail.com>
2018-08-06 17:11:30 +0200

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  # def ask
+  #   @ask = params[:questions]
+  #   @questions = QUESTIONS.select { |id, r| r[:ask] == @ask}
+  # end
+
+  def answer
+    @answer = params[:questions]
+    if @message = "I am going to work"
+      @answer = "Great!"
+    elsif @message.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
